3eb89afa8722cc18a80d65433b98e404805e44a6
godtyze <omgfail301@gmail.com>
2022-05-17 03:13:06 +0600

    feat: complete 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+readableStream.on('data', data => console.log(data.toString()));
+
+

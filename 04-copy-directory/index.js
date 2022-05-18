c715a7fafc025486b420d50f005a638f2290e959
godtyze <omgfail301@gmail.com>
2022-05-19 01:13:47 +0600

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const { mkdir, copyFile, readdir, rm } = require('fs/promises');
 
+const pathToFilesFolder = path.join(__dirname, 'files');
+const pathToCopyFolder = path.join(__dirname, 'files-copy');
+
+async function copyDirectoryFiles(pathToFilesFolder, pathToCopyFolder) {
+  try {
+    await rm(pathToCopyFolder, { recursive: true, force: true });
+    await mkdir(pathToCopyFolder, {recursive: true});
+    const folderFiles = await readdir(pathToFilesFolder, {withFileTypes: true});
+    for (let i = 0; i < folderFiles.length; i++) {
+      if (folderFiles[i].isDirectory()) {
+        copyDirectoryFiles(path.join(pathToFilesFolder, folderFiles[i].name), path.join(pathToCopyFolder, folderFiles[i].name));
+      } else {
+        await copyFile(path.join(pathToFilesFolder, folderFiles[i].name), path.join(pathToCopyFolder, folderFiles[i].name));
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+copyDirectoryFiles(pathToFilesFolder, pathToCopyFolder);
